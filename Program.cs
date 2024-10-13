eba06c67e9115d4460963929c89dcd9585a616a8
Nick <nikngeno@gmail.com>
2024-10-13 16:29:21 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace IsAbecedarian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsAbecedarian("bandeer"));
+            Console.WriteLine(IsAbecedarian("dehort"));
+        }
+        public static bool IsAbecedarian(string word)
+        {
+            char[] letters = word.ToCharArray();
+            for (int i = 0; i < letters.Length - 1; i++)
+            {
+                if (letters[i] > letters[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
